20c30a350bae4bb94d033f6bd7c441bec71a1d7a
André Fonseca Borges <andre.borges@demarco.com.br>
2023-12-27 16:41:28 -0300

    up code

--- Combination.py
+++ Combination.py
@@ -31,13 +31,74 @@ def detectar():
             cv2.rectangle(frame, (x, y), (x + l, y + a), (0, 255, 0), 2)
             cv2.imshow("Deteccao", frame)
 
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
 
-        if x > 0:
-            print('Detecção esfeuada pelo haarcascade!')
-            return x, y, l, a
+            if x > 0:
+                print('Detecção esfeuada pelo haarcascade!')
+                return x, y, l, a
 
 
 bbox = detectar()
-print(bbox)
+# print(bbox)
+
+ok = tracker.init(frame, bbox)
+colors = (randint(0, 255), randint(0, 255), randint(0, 255))
+
+while True:
+    ok, frame = video.read()
+
+    if not ok:
+        break
+
+    ok, bbox = tracker.update(frame)
+
+    if ok:
+        (x, y, w, h) = [int(v) for v in bbox]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), colors, 2, 1)
+    else:
+        print('Falha no rastreamento. Sera executado o detector haarcascade!')
+        bbox = detectar()
+        tracker = cv2.TrackerMOSSE_create()
+        tracker.init(frame, bbox)
+
+    cv2.imshow("Tracking", frame)
+
+    k = cv2.waitKey(1) & 0XFF
+    if k == 27:
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
